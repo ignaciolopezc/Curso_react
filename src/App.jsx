cf57cb8c74ef3e0217b35a0073e0b4a488c574f8
Ignacio Lopez C <ignaciolopezc@gmail.com>
2023-07-07 15:38:37 -0400

    mod app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+const ItemComida = (props) => {
+    return <li>{props.fru}</li>;
+};
 
+const MiBoton = ({texto}) => {
+    const handleclickbutton = (title) => {
+        console.log('funcion de click' + title);
+    }
+    return <button onClick={() =>  handleclickbutton('asdfsadf')} >{texto}</button>
+};
+
+const WelcomeUser = ({user}) => (user ? <h3>Online</h3> : <h3>OFFline</h3>)
 
 
 const App = () => {
     const titulo = 'wena choro';
+    const comida = ["🍕", "🍔", "🍟", "🌭"];
+    const user = false;
+    const boton = 'boton react';
     
     return (
         <>
             <h1 className="text-center">{titulo}</h1>
+            <h1 className="text-center">{user}</h1>
+            <WelcomeUser user={user} />
+            <ul>
+                {
+                    comida.map((fru, index) => (
+                        <ItemComida key={index} fru={fru}/>
+                    ))
+                }
+            </ul>
+            <MiBoton texto={boton} />
         </>
-    )
+    )    
 }
 
 
